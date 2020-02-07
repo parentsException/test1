1760e2c0dea244e42ab9c7ba6cd2dd5ed26bc9da
Administrator <Administrator@PC201506031038>
2020-02-07 14:29:02 +0800

    version 1.01

--- test1/src/test1/Test1.java
+++ test1/src/test1/Test1.java
@@ -2,6 +2,6 @@ package test1;
 
 public class Test1 {
 	public static void main(String[] args) {
-		System.out.println("这是测试分享本地仓库的测试");
+		System.out.println("这是测试分享本地仓库的测试  version   1.01");
 	}
 }
